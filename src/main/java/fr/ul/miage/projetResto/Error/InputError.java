a9d34b460e5f794d67b5986bdc57f2ba543e6e31
Camille <beirao.camille@gmail.com>
2021-04-21 21:05:47 +0200

    feat <InputError> : USNF7.1 gestion des erreus de saisie
    Création des methodes pour vérifier les imput de l'utilisation (integer,
    login, commande, date)

--- src/main/java/fr/ul/miage/projetResto/Error/InputError.java
+++ src/main/java/fr/ul/miage/projetResto/Error/InputError.java
@@ -1,23 +1,79 @@
 package fr.ul.miage.projetResto.Error;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputError {
 
-	public static Integer checkInteger(String test, int borneMin, int borneMax) {
-		return null;
+	public static Integer checkInteger(String nb, Integer borneMin, Integer borneMax) {
+		Integer toReturn;
+		try {
+			toReturn = Integer.parseInt(nb);
+
+		} catch (Exception e) {
+			return null;
+		}
+
+		if (toReturn < borneMin || toReturn > borneMax) {
+			return null;
+		}
+		return toReturn;
 	}
 
-	public static String checkString(String test) {
-		return null;
+	public static String checkUserId(String userId) {
+		String regExpression = "[a-zA-Z_0-9]*";
+		Pattern p = Pattern.compile(regExpression);
+		Matcher matcher = p.matcher(userId);
+
+		if (!matcher.matches())
+			return null;
+
+		return userId;
 	}
 
-	public static String checkUserId(String test) {
-		return null;
+	public static String checkDate(String date) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		format.setLenient(false);
+		try {
+			Date d = format.parse(date);
+		} catch (ParseException e) {
+			return null;
+		}
+		return date;
 	}
 
-	public static Short checkDate(String test) {
-		return null;
+	public static String checkStringCommande(String commande, Integer borneMin, Integer borneMax) {
+		if (commande.charAt(0) != ("-").charAt(0)) {
+			return null;
+		}
+
+
+		String selection = commande.substring(2);
+		String[] listSelection = selection.replace(" ", "").split("/");
+		String action = String.valueOf(commande.charAt(1));
+		switch (action) {
+		case "v":
+			if (commande.substring(2) != "") {
+				return null;
+			}
+			break;
+		case "a":
+		case "e":
+		case "d":
+			for (String part : listSelection) {
+				if (checkInteger(part, borneMin, borneMax) == null) {
+					return null;
+				}
+			}
+			break;
+		default:
+			return null;
+		}
+		return commande;
 	}
-	
-	
 
 }
