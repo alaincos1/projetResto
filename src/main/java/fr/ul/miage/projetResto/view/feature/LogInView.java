58a588566b37ff46e4f2759185569d3b79d54ea9
MoissBatt <valentine.bouche.stjo@gmail.com>
2021-04-22 21:10:35 +0200

    feat : US6.1 - US6.2 Start et LogIn
    L'utilisateur se connecte et entre la date, le type de repas et son identifiant

--- src/main/java/fr/ul/miage/projetResto/view/feature/LogInView.java
+++ src/main/java/fr/ul/miage/projetResto/view/feature/LogInView.java
@@ -1,4 +1,12 @@
 package fr.ul.miage.projetResto.view.feature;
 
 public class LogInView {
+
+    public void displayLogIn() {
+        System.out.println("Veuillez vous connecter avec votre identifiant : ");
+    }
+
+    public void displayDisconnect(){
+        System.out.println("Vous êtes déconnecté.e");
+    }
 }
--- src/main/java/fr/ul/miage/projetResto/view/feature/StartView.java
+++ src/main/java/fr/ul/miage/projetResto/view/feature/StartView.java
@@ -1,5 +1,22 @@
 package fr.ul.miage.projetResto.view.feature;
 
-public class StartView {
-    //afficher une demande de date et type de repas
+import fr.ul.miage.projetResto.Launcher;
+import fr.ul.miage.projetResto.constants.MealType;
+
+public class StartView{
+
+    public void displayMealType() {
+        System.out.println("Veuillez choisir le type de service :");
+        for(MealType mealType : MealType.values()){
+            System.out.println(mealType.getId()+") "+mealType);
+        }
+    }
+
+    public void displayDate() {
+        System.out.println("Veuillez choisir la date du service au format  \"AAAA/MM/JJ\" :");
+    }
+
+    public void displayService() {
+        System.out.println("Service du jour : "+ Launcher.getService().getMealType()+ " " + Launcher.getService().getDate());
+    }
 }
