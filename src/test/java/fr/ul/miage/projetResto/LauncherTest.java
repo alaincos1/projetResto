51216dc92599eb18231300d077bb65988bc0e667
MoissBatt <valentine.bouche.stjo@gmail.com>
2021-04-22 21:04:46 +0200

    test <LauncherTest> US6.2
    Ajouts de tests

--- src/test/java/fr/ul/miage/projetResto/LauncherTest.java
+++ src/test/java/fr/ul/miage/projetResto/LauncherTest.java
@@ -2,9 +2,8 @@ package fr.ul.miage.projetResto;
 
 import fr.ul.miage.projetResto.appinfo.Service;
 import fr.ul.miage.projetResto.constants.MealType;
-import lombok.var;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import fr.ul.miage.projetResto.constants.Role;
+import fr.ul.miage.projetResto.model.User;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,15 +11,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LauncherTest {
 
     @Test
-    void testInitializeService(){
-        Launcher.initializeService(MealType.Dîner, "2021/04/20");
+    void testSetService(){
+        Launcher.setService(MealType.Dîner, "2021/04/20");
         assertEquals(Launcher.getService().getMealType(), MealType.Dîner);
         assertEquals(Launcher.getService().getDate(), "2021/04/20");
     }
 
     @Test
     void testGetService(){
-        Launcher.initializeService(MealType.Déjeuner, "2020/04/20");
+        Launcher.setService(MealType.Déjeuner, "2020/04/20");
         assertEquals(Launcher.getService(), new Service(MealType.Déjeuner, "2020/04/20"));
+    }
+
+    @Test
+    void testSetLoggedUser(){
+        Launcher.setLoggedUser(new User("admin", Role.Director));
+        assertEquals(Launcher.getLoggedUser(),new User("admin", Role.Director));
+    }
+
+    @Test
+    void testGetLoggedUser(){
+        Launcher.setLoggedUser(new User("admin", Role.Butler));
+        assertEquals(Launcher.getLoggedUser(), new User("admin", Role.Butler));
     }
 }
