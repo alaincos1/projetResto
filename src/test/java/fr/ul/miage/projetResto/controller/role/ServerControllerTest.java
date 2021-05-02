bfbfb0b8ede3ceb7f5ea2f6ce218f19aad003460
MoissBatt <valentine.bouche.stjo@gmail.com>
2021-05-03 00:30:39 +0200

    test <ServerControllerTest, OrderEntityTest> : US3.5 Servir une commande
    Création des tests pour servir une commande

--- src/test/java/fr/ul/miage/projetResto/controller/role/ServerControllerTest.java
+++ src/test/java/fr/ul/miage/projetResto/controller/role/ServerControllerTest.java
@@ -1,9 +1,12 @@
 package fr.ul.miage.projetResto.controller.role;
 
 import fr.ul.miage.projetResto.appinfo.Service;
+import fr.ul.miage.projetResto.constants.DishType;
 import fr.ul.miage.projetResto.constants.Role;
 import fr.ul.miage.projetResto.constants.TableState;
 import fr.ul.miage.projetResto.dao.service.BaseService;
+import fr.ul.miage.projetResto.model.entity.DishEntity;
+import fr.ul.miage.projetResto.model.entity.OrderEntity;
 import fr.ul.miage.projetResto.model.entity.TableEntity;
 import fr.ul.miage.projetResto.model.entity.UserEntity;
 import fr.ul.miage.projetResto.view.role.ServerView;
@@ -112,5 +115,104 @@ class ServerControllerTest {
         verify(serverView, times(1)).displayTablesToDirty(anyList());
         verify(baseService, times(0)).update(any(TableEntity.class));
         assertEquals(TableState.Starter, tables.get(0).getTableState());
+    }
+
+    @Test
+    @DisplayName("Servir une commande")
+    void checkServeOrders() {
+        UserEntity user = new UserEntity();
+        user.set_id("ser1");
+        List<TableEntity> tables = new ArrayList<>();
+        List<OrderEntity> orders = new ArrayList<>();
+        List<String> dishes = new ArrayList<>();
+        dishes.add("plat");
+        for(int i = 0; i < 3; i++){
+            OrderEntity order = new OrderEntity();
+            order.set_id(""+i);
+            order.setIdTable(""+i);
+            order.setIdsDish(dishes);
+            orders.add(order);
+        }
+        DishEntity dishEntity = new DishEntity();
+        dishEntity.setDishType(DishType.Starter);
+        when(baseService.getAllTableByServerOrHelper(anyString())).thenReturn(tables);
+        doReturn(orders).when(serverController).getOnlyServerOrders(anyList(), anyList());
+        doReturn(1).when(serverController).getIntegerInput(anyInt(), anyInt());
+        when(baseService.getTableById(anyString())).thenReturn(new TableEntity());
+        when(baseService.getDishById(anyString())).thenReturn(dishEntity);
+        doNothing().when(serverController).launch(Role.Server);
+
+        serverController.serveOrders(user);
+
+        verify(serverView, times(1)).displayOrdersToServe(anyList());
+        verify(baseService, times(1)).update(any(OrderEntity.class));
+    }
+
+    @Test
+    @DisplayName("Annuler le service d'une commande")
+    void checkServeOrdersCancel() {
+        UserEntity user = new UserEntity();
+        user.set_id("ser1");
+        List<TableEntity> tables = new ArrayList<>();
+        List<OrderEntity> orders = new ArrayList<>();
+        List<String> dishes = new ArrayList<>();
+        dishes.add("plat");
+        for(int i = 0; i < 3; i++){
+            OrderEntity order = new OrderEntity();
+            order.set_id(""+i);
+            order.setIdTable(""+i);
+            order.setIdsDish(dishes);
+            orders.add(order);
+        }
+        DishEntity dishEntity = new DishEntity();
+        dishEntity.setDishType(DishType.Starter);
+        when(baseService.getAllTableByServerOrHelper(anyString())).thenReturn(tables);
+        doReturn(orders).when(serverController).getOnlyServerOrders(anyList(), anyList());
+        doReturn(0).when(serverController).getIntegerInput(anyInt(), anyInt());
+        doNothing().when(serverController).launch(Role.Server);
+
+        serverController.serveOrders(user);
+
+        verify(serverView, times(1)).displayOrdersToServe(anyList());
+        verify(baseService, times(0)).update(any(OrderEntity.class));
+    }
+
+    @Test
+    @DisplayName("Aucune commande à servir")
+    void checkServeOrdersNone() {
+        UserEntity user = new UserEntity();
+        user.set_id("ser1");
+        List<TableEntity> tables = new ArrayList<>();
+        List<OrderEntity> orders = new ArrayList<>();
+        when(baseService.getAllTableByServerOrHelper(anyString())).thenReturn(tables);
+        doReturn(orders).when(serverController).getOnlyServerOrders(anyList(), anyList());
+        doNothing().when(serverController).launch(Role.Server);
+
+        serverController.serveOrders(user);
+
+        verify(serverView, times(1)).displayNoOrdersToServe();
+        verify(baseService, times(0)).update(any(OrderEntity.class));
+    }
+
+    @Test
+    @DisplayName("Récupère uniquement les commandes de cet utilisateur")
+    void checkGetOnlyServerOrders() {
+        List<OrderEntity> orders = new ArrayList<>();
+        List<OrderEntity> orderExpected = new ArrayList<>();
+        OrderEntity order = null;
+        for(int i = 0; i < 3; i++){
+            order = new OrderEntity();
+            order.setIdTable(""+i);
+            orders.add(order);
+        }
+        orderExpected.add(order);
+
+        List<TableEntity> tables = new ArrayList<>();
+        TableEntity table = new TableEntity();
+        table.set_id("2");
+        tables.add(table);
+
+        List<OrderEntity> ordersActual = serverController.getOnlyServerOrders(orders, tables);
+        assertEquals(orderExpected, ordersActual);
     }
 }
---
+++ src/test/java/fr/ul/miage/projetResto/model/entity/OrderEntityTest.java
@@ -0,0 +1,105 @@
+package fr.ul.miage.projetResto.model.entity;
+
+import fr.ul.miage.projetResto.constants.DishType;
+import fr.ul.miage.projetResto.constants.TableState;
+import fr.ul.miage.projetResto.dao.service.BaseService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+@DisplayName("OrderEntity")
+@ExtendWith(MockitoExtension.class)
+class OrderEntityTest {
+    @InjectMocks
+    OrderEntity orderEntity;
+
+    @Test
+    @DisplayName("Retourne TableState.Starter")
+    void checkGetDishTypeStarter(@Mock BaseService baseService) {
+        orderEntity = new OrderEntity();
+
+        List<String> dishes = new ArrayList<>();
+        dishes.add("boisson");
+        dishes.add("salade");
+        orderEntity.setIdsDish(dishes);
+
+        DishEntity dishEntity = new DishEntity();
+        dishEntity.setDishType(DishType.Drink);
+        DishEntity dishEntity1 = new DishEntity();
+        dishEntity1.setDishType(DishType.Starter);
+        when(baseService.getDishById(anyString())).thenReturn(dishEntity).thenReturn(dishEntity1);
+
+        TableState tableState = orderEntity.getDishType(baseService);
+        assertEquals(TableState.Starter,tableState);
+    }
+
+    @Test
+    @DisplayName("Retourne TableState.MainCourse")
+    void checkGetDishTypeMainCourse(@Mock BaseService baseService) {
+        orderEntity = new OrderEntity();
+
+        List<String> dishes = new ArrayList<>();
+        dishes.add("boisson");
+        dishes.add("légumes");
+        orderEntity.setIdsDish(dishes);
+
+        DishEntity dishEntity = new DishEntity();
+        dishEntity.setDishType(DishType.Drink);
+        DishEntity dishEntity1 = new DishEntity();
+        dishEntity1.setDishType(DishType.MainCourse);
+        when(baseService.getDishById(anyString())).thenReturn(dishEntity).thenReturn(dishEntity1);
+
+        TableState tableState = orderEntity.getDishType(baseService);
+        assertEquals(TableState.MainCourse,tableState);
+    }
+
+    @Test
+    @DisplayName("Retourne TableState.Dessert")
+    void checkGetDishType(@Mock BaseService baseService) {
+        orderEntity = new OrderEntity();
+
+        List<String> dishes = new ArrayList<>();
+        dishes.add("boisson");
+        dishes.add("beignet au chocolat");
+        orderEntity.setIdsDish(dishes);
+
+        DishEntity dishEntity = new DishEntity();
+        dishEntity.setDishType(DishType.Drink);
+        DishEntity dishEntity1 = new DishEntity();
+        dishEntity1.setDishType(DishType.Dessert);
+        when(baseService.getDishById(anyString())).thenReturn(dishEntity).thenReturn(dishEntity1);
+
+        TableState tableState = orderEntity.getDishType(baseService);
+        assertEquals(TableState.Dessert,tableState);
+    }
+
+    @Test
+    @DisplayName("Retourne null : normalement n'arrive jamais")
+    void checkGetDishTypeNone(@Mock BaseService baseService) {
+        orderEntity = new OrderEntity();
+
+        List<String> dishes = new ArrayList<>();
+        dishes.add("boisson");
+        orderEntity.setIdsDish(dishes);
+
+        DishEntity dishEntity = new DishEntity();
+        dishEntity.setDishType(DishType.Drink);
+        when(baseService.getDishById(anyString())).thenReturn(dishEntity);
+
+        TableState tableState = orderEntity.getDishType(baseService);
+        assertNull(tableState);
+    }
+}
