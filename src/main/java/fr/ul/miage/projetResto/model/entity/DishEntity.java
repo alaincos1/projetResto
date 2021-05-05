0fbc6277e3e81ab5b783fce6fa595b2278e384fc
MoissBatt <valentine.bouche.stjo@gmail.com>
2021-05-05 12:10:37 +0200

    feat <DishEntity> : USNF5 Gestion des stocks lors de l'ajout d'un plat ou son retrait
    Méthode checkStock qui annonce si le stock d'une ressource est suffisante
    Méthode changeStock qui ajoute ou retire du stock aux produits du plat

--- src/main/java/fr/ul/miage/projetResto/model/entity/DishEntity.java
+++ src/main/java/fr/ul/miage/projetResto/model/entity/DishEntity.java
@@ -1,6 +1,7 @@
 package fr.ul.miage.projetResto.model.entity;
 
 import fr.ul.miage.projetResto.constants.DishType;
+import fr.ul.miage.projetResto.dao.service.BaseService;
 import lombok.Data;
 
 import java.util.List;
@@ -26,4 +27,28 @@ public class DishEntity {
         }
         return toString.toString();
     }
+
+    //Retourne true si tous les produits du plat ont le stock suffisant
+    protected boolean checkStock(BaseService baseService) {
+        for (String product : idsProduct) {
+            if (baseService.getProductById(product).getStock() == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Ajoute 1 au stock de chaque produit si add = true sinon retire 1
+    protected void changeStock(BaseService baseService, boolean add) {
+        for (String product : idsProduct) {
+            ProductEntity productEntity = baseService.getProductById(product);
+            if (add) {
+                productEntity.setStock(productEntity.getStock() + 1);
+            } else {
+                productEntity.setStock(productEntity.getStock() - 1);
+            }
+            baseService.save(productEntity);
+        }
+    }
+
 }
